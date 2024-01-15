b7faf341051094916a9048a40fbb78cbbd142c88
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2024-01-15 17:52:01 +0200

    Add validation to login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -8,6 +8,22 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  var _email = '';
+  var _password = '';
+  void _onSubmit() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print(_email);
+      print(_password);
+    } else {}
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,9 +53,20 @@ class _LoginScreenState extends State<LoginScreen> {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Form(
+                        key: _formKey,
                         child: Column(
                           children: [
                             TextFormField(
+                              validator: (value) {
+                                if (value == null || !value.contains('@')) {
+                                  return 'Please enter a valid email';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              onSaved: (newValue) {
+                                _email = newValue!;
+                              },
                               onTapOutside: (event) {
                                 FocusManager.instance.primaryFocus?.unfocus();
                               },
@@ -51,6 +78,16 @@ class _LoginScreenState extends State<LoginScreen> {
                               autocorrect: false,
                             ),
                             TextFormField(
+                              validator: (value) {
+                                if (value == null || value.trim().length <= 6) {
+                                  return 'Password must be at least 7 characters';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              onSaved: (newValue) {
+                                _password = newValue!;
+                              },
                               onTapOutside: (event) {
                                 FocusManager.instance.primaryFocus?.unfocus();
                               },
@@ -61,7 +98,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             ),
                             const SizedBox(height: 30),
                             ElevatedButton(
-                              onPressed: () {},
+                              onPressed: _onSubmit,
                               style: ElevatedButton.styleFrom(
                                   backgroundColor: const Color(0xfffab714)),
                               child: const SizedBox(
@@ -72,7 +109,22 @@ class _LoginScreenState extends State<LoginScreen> {
                                   textAlign: TextAlign.center,
                                 ),
                               ),
-                            )
+                            ),
+                            const SizedBox(height: 10),
+                            RichText(
+                              text: const TextSpan(
+                                text: "Don't have email?",
+                                style: TextStyle(color: Colors.black),
+                                children: [
+                                  TextSpan(
+                                    text: " create new one",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ],
                         ),
                       )
