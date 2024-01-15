95d6e0e33ec5baf1160f243be7a4f741c8ea2a73
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2024-01-15 17:07:08 +0200

    Add login screen ui

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,10 +1,89 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pulse Talk'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                top: 30,
+                left: 65,
+                right: 65,
+                bottom: 20,
+              ),
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              color: Colors.grey.shade300,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Form(
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              onTapOutside: (event) {
+                                FocusManager.instance.primaryFocus?.unfocus();
+                              },
+                              decoration: const InputDecoration(
+                                labelText: 'Email Address',
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              textCapitalization: TextCapitalization.none,
+                              autocorrect: false,
+                            ),
+                            TextFormField(
+                              onTapOutside: (event) {
+                                FocusManager.instance.primaryFocus?.unfocus();
+                              },
+                              decoration: const InputDecoration(
+                                // border: OutlineInputBorder(),
+                                label: Text('Password'),
+                              ),
+                            ),
+                            const SizedBox(height: 30),
+                            ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xfffab714)),
+                              child: const SizedBox(
+                                width: double.infinity,
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(color: Colors.black),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
