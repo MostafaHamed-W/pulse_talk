a29bf5fae63de360020ee48d6d6bd248bd98c207
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2024-01-20 20:12:24 +0200

    Update chat scren with stream builder

--- lib/screens/chat_messages.dart
+++ lib/screens/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +6,36 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No messages yet'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'createdAt',
+            descending: false,
+          )
+          .snapshots(),
+      builder: (context, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Text('No messages yet');
+        }
+        if (chatSnapshots.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        final loadedMessages = chatSnapshots.data!.docs;
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(loadedMessages[index]['text']),
+            );
+          },
+        );
+      },
     );
   }
 }
